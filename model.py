e0a2569cd685c42f883ea6a0367aee2621f1b9e7
Tiyasa-Mukherjee76 <2205425@kiit.ac.in>
2025-06-22 22:05:32 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,18 @@
 #trained model
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torchvision import datasets, transforms
 
-# Load MNIST dataset
-transform = transforms.ToTensor()
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('.', train=True, download=True, transform=transform),
-    batch_size=64, shuffle=True)
+class Generator(nn.Module):
+    def __init__(self, latent_dim, num_classes):
+        super().__init__()
+        self.label_emb = nn.Embedding(num_classes, num_classes)
+        self.model = nn.Sequential(
+            nn.Linear(latent_dim + num_classes, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 784),
+            nn.Tanh()
+        )
+
+    def forward(self, noise, labels):
+        x = torch.cat((self.label_emb(labels), noise), -1)
+        return self.model(x).view(-1, 1, 28, 28)
