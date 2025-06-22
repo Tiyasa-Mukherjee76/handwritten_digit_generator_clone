9f0a03fd6d24bfd95287e7bf6919c0885888f4b4
Tiyasa-Mukherjee76 <2205425@kiit.ac.in>
2025-06-22 22:07:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,3 @@
-# Simple Generator model
-class Generator(nn.Module):
-    def __init__(self, latent_dim, num_classes):
-        super().__init__()
-        self.label_emb = nn.Embedding(num_classes, num_classes)
-        self.model = nn.Sequential(
-            nn.Linear(latent_dim + num_classes, 128),
-            nn.ReLU(True),
-            nn.Linear(128, 784),
-            nn.Tanh()
-        )
-
-    def forward(self, noise, labels):
-        x = torch.cat((self.label_emb(labels), noise), -1)
-        return self.model(x).view(-1, 1, 28, 28)
-
-# Instantiate
-latent_dim = 100
-num_classes = 10
-generator = Generator(latent_dim, num_classes)
-
 #app.py
 import torch
 import streamlit as st
